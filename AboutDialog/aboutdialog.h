8014530803fa7a5ae0e4435f808be0a46eddbe3d
Oakware <ivan@ioMac.local>
2016-07-25 21:36:44 +0300

    Update

--- AboutDialog/aboutdialog.h
+++ AboutDialog/aboutdialog.h
@@ -1,7 +1,31 @@
+/*****************************************************************************
+ * Copyright © 2015 Oakware - All Rights Reserved
+ *
+ * Github : <https://github.com/Oakware>
+ * Authors: Ivan Kondratuk <i.o.kondratuk@gmail.com>
+ *
+ * This file is part of Graph Builder.
+ *
+ * Graph Builder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graph Builder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graph Builder.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
 #ifndef ABOUTDIALOG_H
 #define ABOUTDIALOG_H
 
 #include <QDialog>
+#include <QDesktopServices>
+#include <QUrl>
 #include "ui_aboutdialog.h"
 
 class AboutDialog : public QDialog{
@@ -17,6 +41,11 @@ public:
 	}
 
 	~AboutDialog(){delete ui;}
+
+private slots:
+	void on_label_linkActivated(const QString &link){
+		QDesktopServices::openUrl(link);
+	}
 
 private:
 	Ui::AboutDialog *ui;
--- GraphView/graphview.cpp
+++ GraphView/graphview.cpp
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * Copyright © 2015 Oakware - All Rights Reserved
+ *
+ * Github : <https://github.com/Oakware>
+ * Authors: Ivan Kondratuk <i.o.kondratuk@gmail.com>
+ *
+ * This file is part of Graph Builder.
+ *
+ * Graph Builder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graph Builder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graph Builder.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
 #include "graphview.h"
 
 GraphView::GraphView(QWidget *parent):
@@ -299,7 +321,7 @@ void GraphView::mousePressEvent(QMouseEvent *ev){
 	if(!(isEditable && Graph)){return;}
 
 	CurX = ev->x();				// Set selected
-	CurY = ev->y();	
+	CurY = ev->y();
 
 	if(ev->button() != Qt::LeftButton){
 		if(CurrentState == Normal)
